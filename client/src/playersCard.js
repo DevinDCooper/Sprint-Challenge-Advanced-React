9d2a40eababdae81d13536db31b8f03f7de5a6d9
Devin Cooper <37602228+DevinDCooper@users.noreply.github.com>
2020-03-20 20:58:07 -0700

    dark commit

---
+++ client/src/playersCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Card,  CardText, CardBody,
+  CardTitle, CardSubtitle, 
+} from 'reactstrap';
+
+const PlayersCard = ({data}) => {
+  return (
+    <div className="big-box">
+    <div className ="play-card">
+      <Card>
+        <CardBody>
+          <CardTitle>Name: {data.name}</CardTitle>
+        <br/>
+          <CardSubtitle>Country: {data.country}</CardSubtitle>
+          <CardText>Searches: {data.searches}</CardText>
+          <CardText>ID: {data.id}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+    </div>
+  );
+};
+
+export default PlayersCard;
+
+
